5f77b58be9fca1f67bda557262f196b58bc677e8
Denis Bazhin <den.bazhin.2017@mail.ru>
2024-02-28 11:33:41 +0500

    homework 6.8 - task1 done.
    Добавлены статические переменные totalItemCount и totalBasketCount.
    
    Реализованы статические методы increaseTotalItemCount, increaseTotalCost, и increaseTotalBasketCount для увеличения соответствующих переменных.
    
     Добавлен статический метод calculateAverageBasketCost для расчета средней стоимости корзины.

--- ObjectsAndClasses_Methods/src/Basket.java
+++ ObjectsAndClasses_Methods/src/Basket.java
@@ -1,14 +1,16 @@
 public class Basket {
-    private static int totalCount = 0;
+    private static int totalItemCount = 0;
     private static int totalCost = 0;
+    private static int totalBasketCount = 0;
 
     private String items = "";
     private int totalPrice = 0;
     private int limit;
 
     public Basket() {
-        increaseCount(1);
-        items = "Список продуктов:";
+        increaseTotalItemCount(1);
+        increaseTotalBasketCount(1);
+        items = "List of products:";
         this.limit = 1000000;
     }
 
@@ -23,34 +25,42 @@ public class Basket {
         this.totalPrice = totalPrice;
     }
 
-    public static int getTotalCount() {
-        return totalCount;
+    public static int getTotalItemCount() {
+        return totalItemCount;
     }
 
     public static int getTotalCost() {
         return totalCost;
     }
 
-    public static void increaseCount(int count) {
-        totalCount = totalCount + count;
+    public static int getTotalBasketCount() {
+        return totalBasketCount;
     }
 
-    public static void increaseCost(int cost) {
+    public static void increaseTotalItemCount(int count) {
+        totalItemCount = totalItemCount + count;
+    }
+
+    public static void increaseTotalCost(int cost) {
         totalCost = totalCost + cost;
     }
 
-    public static double calculateAveragePrice() {
-        if (totalCount == 0) {
-            return 0;
-        }
-        return (double) totalCost / totalCount;
+    public static void increaseTotalBasketCount(int count) {
+        totalBasketCount = totalBasketCount + count;
     }
 
-    public static double calculateAverageBasketCost(int numberOfBaskets) {
-        if (numberOfBaskets == 0) {
+    public static double calculateAveragePrice() {
+        if (totalItemCount == 0) {
             return 0;
         }
-        return (double) totalCost / numberOfBaskets;
+        return (double) totalCost / totalItemCount;
+    }
+
+    public static double calculateAverageBasketCost() {
+        if (totalBasketCount == 0) {
+            return 0;
+        }
+        return (double) totalCost / totalBasketCount;
     }
 
     public void add(String name, int price) {
@@ -68,15 +78,15 @@ public class Basket {
         }
 
         if (error) {
-            System.out.println("Возникла ошибка :(");
+            System.out.println("Error occurred :(");
             return;
         }
 
         items = items + "\n" + name + " - " +
-                count + " шт. - " + price + "р.";
+                count + " шт. - " + price;
         totalPrice = totalPrice + count * price;
-        increaseCount(count);
-        increaseCost(count * price);
+        increaseTotalItemCount(count);
+        increaseTotalCost(count * price);
     }
 
     public void clear() {
@@ -95,7 +105,7 @@ public class Basket {
     public void print(String title) {
         System.out.println(title);
         if (items.isEmpty()) {
-            System.out.println("Корзина пуста!");
+            System.out.println("Cart is empty");
         } else {
             System.out.println(items);
         }
