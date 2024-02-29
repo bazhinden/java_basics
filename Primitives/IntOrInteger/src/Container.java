1de832c4781ea5ac0c604151f1cc93cc7702d13f
Denis Bazhin <den.bazhin.2017@mail.ru>
2024-02-29 12:33:46 +0500

    homework 7.9_task1 - done. Проинициализировал значение "count".

--- Primitives/IntOrInteger/src/Container.java
+++ Primitives/IntOrInteger/src/Container.java
@@ -2,10 +2,16 @@ public class Container {
     private Integer count;
 
     public void addCount(int value) {
+        if (count == null) {
+            count = 0;
+        }
         count = count + value;
     }
 
     public int getCount() {
+        if (count == null) {
+            return 0;
+        }
         return count;
     }
 }
