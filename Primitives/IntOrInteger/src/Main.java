14cf4377e077cffb9caaa9792a88e77a7add1e2d
Denis Bazhin <den.bazhin.2017@mail.ru>
2024-02-29 12:51:47 +0500

    homework 7.9_task2 - done. Добавил 3 цикла, которые выводят все числовые коды букв русского алфавита — заглавные, строчные и букву "Ё".

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -4,9 +4,22 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+        System.out.println("Числовые коды букв русского алфавита.");
+        System.out.println("Заглавные буквы:");
+        for (char ch = 'А'; ch <= 'Я'; ch++) {
+            int code = (int) ch;
+            System.out.println(ch + ": " + code);
+        }
+
+        System.out.println("Строчные буквы:");
+        for (char ch = 'а'; ch <= 'я'; ch++) {
+            int code = (int) ch;
+            System.out.println(ch + ": " + code);
+        }
+
+        char letterYo = 'Ё';
+        int codeYo = (int) letterYo;
+        System.out.println("Буква " + letterYo + ": " + codeYo);
 
     }
 }
