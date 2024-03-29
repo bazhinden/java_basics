a2f5aa46e03957a59ec4f4cdff70a11be5159f4a
Denis Bazhin <den.bazhin.2017@mail.ru>
2024-03-29 11:53:31 +0500

    18.9 homework - done

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,37 +1,83 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
+    private static final Pattern EMAIL_PATTERN =
+            Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+    private static final Pattern PHONE_PATTERN =
+            Pattern.compile("^\\+?[0-9]+$");
+
+    private static final Logger LOGGER = LogManager.getLogger(CustomerStorage.class);
+    private static final Marker INVALID_INPUT_MARKER = MarkerManager.getMarker("INVALID_INPUT");
+    private static final Marker STORAGE_OPERATION_MARKER = MarkerManager.getMarker("STORAGE_OPERATION");
 
     public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data) {
+    public void addCustomer(String data) throws IllegalArgumentException {
         final int INDEX_NAME = 0;
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
 
-        String[] components = data.split("\\s+");
-        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
-        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+        try {
+            String[] components = data.split("\\s+");
+            if (components.length != 4) {
+                throw new IllegalArgumentException("Неправильное количество компонентов во входных данных");
+            }
+            String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+            if (!validateEmail(components[INDEX_EMAIL])) {
+                throw new IllegalArgumentException("Неверный формат электронной почты");
+            }
+
+            if (!validatePhone(components[INDEX_PHONE])) {
+                throw new IllegalArgumentException("Неверный формат номера телефона");
+            }
+            storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+        } catch (IllegalArgumentException e) {
+            LOGGER.error(INVALID_INPUT_MARKER, "Ошибка при добавлении клиента: {}", e.getMessage());
+            throw e;
+        }
     }
 
     public void listCustomers() {
         storage.values().forEach(System.out::println);
+        LOGGER.info(STORAGE_OPERATION_MARKER, "Список клиентов: ");
     }
 
     public void removeCustomer(String name) {
         storage.remove(name);
+        LOGGER.info(STORAGE_OPERATION_MARKER, "Удаленный клиент: ");
     }
 
     public Customer getCustomer(String name) {
-        return storage.get(name);
+        Customer customer = storage.get(name);
+        if (customer == null) {
+            LOGGER.warn(INVALID_INPUT_MARKER, "Клиент с именем \"{}\" не найден", name);
+        }
+        return customer;
     }
 
     public int getCount() {
         return storage.size();
+    }
+
+    private boolean validateEmail(String email) {
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean validatePhone(String phone) {
+        Matcher matcher = PHONE_PATTERN.matcher(phone);
+        return matcher.matches();
     }
 }
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,13 +1,22 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+
 import java.util.Scanner;
 
 public class Main {
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
+    private static final String EXIT_COMMAND = "\texit";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
-            "\tlist\n\tcount\n\tremove Василий Петров";
-    private static final String COMMAND_ERROR = "Wrong command! Available command examples: \n" +
-            COMMAND_EXAMPLES;
+            "\tlist\n\tcount\n\tremove Василий Петров\n" + EXIT_COMMAND;
+    private static final String COMMAND_ERROR = "Wrong command! Available command examples: \n" + COMMAND_EXAMPLES;
     private static final String helpText = "Command examples:\n" + COMMAND_EXAMPLES;
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+    private static final Marker INPUT_HISTORY_MARKER = MarkerManager.getMarker("INPUT_HISTORY");
+    private static final Marker INVALID_STATIONS_MARKER = MarkerManager.getMarker("INVALID_STATIONS");
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,18 +26,39 @@ public class Main {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
 
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+            switch (tokens[0]) {
+                case "add":
+                    try {
+                        executor.addCustomer(tokens[1]);
+                        LOGGER.info(INPUT_HISTORY_MARKER, "Добавлен новый клиент");
+                    } catch (IllegalArgumentException e) {
+                        LOGGER.error(INVALID_STATIONS_MARKER, "Не удалось добавить нового клиента: {}", e.getMessage());
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "list":
+                    executor.listCustomers();
+                    LOGGER.info(INPUT_HISTORY_MARKER, "Список клиентов: ");
+                    break;
+                case "remove":
+                    executor.removeCustomer(tokens[1]);
+                    LOGGER.info(INPUT_HISTORY_MARKER, "Удаленный клиент: ");
+                    break;
+                case "count":
+                    System.out.println("Количество клиентов: " + executor.getCount());
+                    LOGGER.info(INPUT_HISTORY_MARKER, "Количество клиентов: ");
+                    break;
+                case "help":
+                    System.out.println(helpText);
+                    LOGGER.info(INPUT_HISTORY_MARKER, "Справочная информация: ");
+                    break;
+                case "exit":
+                    System.out.println("Выход...");
+                    return;
+                default:
+                    System.out.println(COMMAND_ERROR);
+                    LOGGER.warn(INVALID_STATIONS_MARKER, "Неизвестная команда: {}", command);
+                    break;
             }
         }
     }
