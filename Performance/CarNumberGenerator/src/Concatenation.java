878d9b083a27c53a8b3de310914eb074d92b3080
Denis Bazhin <den.bazhin.2017@mail.ru>
2024-06-22 10:35:25 +0500

    27.18 homework - done.

--- Performance/CarNumberGenerator/src/Concatenation.java
+++ Performance/CarNumberGenerator/src/Concatenation.java
@@ -4,9 +4,9 @@ public class Concatenation {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
 
-        String str = "";
+        StringBuilder str = new StringBuilder();
         for (int i = 0; i < 20_000; i++) {
-            str += "some text some text some text";
+            str.append(" some text some text some text");
         }
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,41 +1,69 @@
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Loader {
 
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        ExecutorService service = Executors.newFixedThreadPool(2);
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
-                    }
-                }
+        service.execute(() -> {
+            try {
+                generateNumbers(1, 100, "res/n1.txt");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        });
+
+        service.execute(() -> {
+            try {
+                generateNumbers(101, 200, "res/n2.txt");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        service.shutdown();
+        try {
+            if (!service.awaitTermination(1, TimeUnit.HOURS)) {
+                System.out.println("Не удалось завершить выполнение задач в течении заданного времени.");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        writer.flush();
-        writer.close();
-
         System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 
-    private static String padNumber(int number, int numberLength) {
-        String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
+    public static void generateNumbers(int regionStart, int regionEnd, String fileName) throws IOException {
+        try (FileOutputStream writer = new FileOutputStream(fileName)) {
+            char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+            for (int number = 1; number < 1000; number++) {
+                int regionCode = 199;
+                for (char firstLetter : letters) {
+                    for (char secondLetter : letters) {
+                        for (char thirdLetter : letters) {
+                            String carNumber = firstLetter + padNumber(number, 3) +
+                                    secondLetter + thirdLetter + padNumber(regionCode, 2);
+                            writer.write(carNumber.getBytes());
+                            writer.write('\n');
+                        }
+                    }
+                }
+            }
+            writer.flush();
         }
+    }
 
-        return numberStr;
+    private static String padNumber(int number, int numberLength) {
+        StringBuilder numberStr = new StringBuilder(Integer.toString(number));
+        while (numberStr.length() < numberLength) {
+            numberStr.insert(0, '0');
+        }
+        return numberStr.toString();
     }
 }
+
